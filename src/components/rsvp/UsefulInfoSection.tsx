684d496f5bd0906283102e4ba738cb9a3be85cbb
Pawel Jablonski <pawel_jablonski@epam.com>
2026-01-23 16:36:54 +0100

    Fix dashes

--- src/components/rsvp/UsefulInfoSection.tsx
+++ src/components/rsvp/UsefulInfoSection.tsx
@@ -4,10 +4,10 @@ const UsefulInfoSection = () => {
         <section className="section">
             <h2>Przydatne informacje</h2>
             <p className="mb-1">
-                Wesele nad jeziorem rządzi się swoimi prawami — teren jest naturalny, więc wysokie obcasy mogą być wyzwaniem.<br />Wybierzcie raczej wygodny, nieprzesadnie elegancki strój.
+                Wesele nad jeziorem rządzi się swoimi prawami - teren jest naturalny, więc wysokie obcasy mogą być wyzwaniem.<br />Wybierzcie raczej wygodny, nieprzesadnie elegancki strój.
             </p>
             <p className="mb-1">
-                Jeśli pragniecie obdarzyć nas prezentem, niech zmieści się w kopercie — to dla nas największa wygoda i radość.
+                Jeśli pragniecie obdarzyć nas prezentem, niech zmieści się w kopercie - to dla nas największa wygoda i radość.
             </p>
         </section>
     );
